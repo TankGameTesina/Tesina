605b6686c8ed2bc5be5f23cf6517ee13e4e1bb88
Michelotti Davide <davidemichelotti@email.com>
2018-04-16 10:07:56 +0200

    Risolto problema orientamento carro armato con sprite

---
+++ 18-04-12_MainGameWithSprite/MainGame/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using SpriteLibrary;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainGame
+{
+    class Bullet
+    {
+        public Point posIniziale;
+        public Sprite colpo;
+        public int distanza;
+        public double grade;
+
+        public Bullet(Point posIniziale, int distanza, double grade, Sprite colpo)
+        {
+            this.posIniziale = posIniziale;
+            this.distanza = distanza;
+            this.grade = grade;
+            this.colpo = colpo;
+        }
+    }
+}
