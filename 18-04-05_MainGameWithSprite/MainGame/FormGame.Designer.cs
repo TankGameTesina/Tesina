a4b4e513ef82616a7d97dbab329028e40c27d82c
Michelotti Davide <davidemichelotti@email.com>
2018-04-16 10:03:54 +0200

    Aggiornamento MainGame con aggiunta gestione sprite tramite la libreria SpriteLibrary

---
+++ 18-04-05_MainGameWithSprite/MainGame/FormGame.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace MainGame
+{
+    partial class FormGame
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrSparo = new System.Windows.Forms.Timer(this.components);
+            this.MainDrawingArea = new System.Windows.Forms.PictureBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.MainDrawingArea)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tmrSparo
+            // 
+            this.tmrSparo.Enabled = true;
+            this.tmrSparo.Interval = 10;
+            this.tmrSparo.Tick += new System.EventHandler(this.tmrSparo_Tick);
+            // 
+            // MainDrawingArea
+            // 
+            this.MainDrawingArea.BackColor = System.Drawing.Color.Gainsboro;
+            this.MainDrawingArea.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainDrawingArea.Location = new System.Drawing.Point(0, 0);
+            this.MainDrawingArea.Name = "MainDrawingArea";
+            this.MainDrawingArea.Size = new System.Drawing.Size(949, 507);
+            this.MainDrawingArea.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.MainDrawingArea.TabIndex = 3;
+            this.MainDrawingArea.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(586, 3);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(363, 498);
+            this.listBox1.TabIndex = 4;
+            // 
+            // FormGame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(949, 507);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.MainDrawingArea);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormGame";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.MainDrawingArea)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer tmrSparo;
+        private System.Windows.Forms.PictureBox MainDrawingArea;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ 18-04-05_MainGameWithSprite/MainGame/FormGame.cs
@@ -0,0 +1,218 @@
+﻿using GraficaTesina;
+using SpriteLibrary;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainGame
+{
+    public partial class FormGame : Form
+    {
+        GenerateTank genTank;
+        List<Bitmap> tank;
+        List<Bitmap> tankEnemy;
+        bool settato = false;
+        double grade = 0;
+        int spostamento =5;
+        int centroCarroX;
+        int centroCarroY;
+        bool newColpo = false;
+        List<Bullet> bullets = new List<Bullet>();
+
+        SpriteController MySpriteController;
+        Sprite JellyMonster;
+        Sprite enemy;
+
+        Point puntoInizioSparo;
+        public FormGame()
+        {
+            InitializeComponent();
+
+            MouseMove += OnMouseMove;
+            HookMouseMove(this.Controls);
+            tank = new List<Bitmap>();
+            genTank = new GenerateTank();
+
+            Random rnd = new Random();
+            int red = rnd.Next(255); // creates a number between 1 and 12
+            int green = rnd.Next(255);  // creates a number between 1 and 6
+            int blue = rnd.Next(255);    // creates a number between 0 and 51
+            genTank.generate(Color.FromArgb(red, green, blue));
+            this.MouseClick += mouseClick;
+            tank = genTank.GetTank();
+            settato = true;
+
+            MainDrawingArea.BackgroundImageLayout = ImageLayout.Stretch;
+
+            MySpriteController = new SpriteController(MainDrawingArea);
+
+            JellyMonster = new Sprite(MySpriteController, tank[0]);
+
+            JellyMonster.SetName("jelly");
+
+
+            JellyMonster.AutomaticallyMoves = false;
+            JellyMonster.CannotMoveOutsideBox = true;
+            JellyMonster.PutBaseImageLocation(new Point(0, 0));
+            JellyMonster.MovementSpeed = 30;
+
+            generateEnemy();
+
+        }
+
+        private void generateEnemy()
+        {
+            Random rnd = new Random();
+            int red = rnd.Next(255); // creates a number between 1 and 12
+            int green = rnd.Next(255);  // creates a number between 1 and 6
+            int blue = rnd.Next(255);    // creates a number between 0 and 51
+            genTank.generate(Color.FromArgb(red, green, blue));
+            tankEnemy = genTank.GetTank();
+
+            enemy = new Sprite(MySpriteController, tankEnemy[0]);
+
+            enemy.SetName("enemy");
+
+            enemy.AutomaticallyMoves = false;
+            enemy.CannotMoveOutsideBox = true;
+            enemy.PutBaseImageLocation(new Point(100, 100));
+            enemy.MovementSpeed = 30;
+        }
+
+        /*
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle |= 0x02000000;
+                return cp;
+            }
+        }
+        */
+        private void HookMouseMove(Control.ControlCollection ctls)
+        {
+            foreach (Control ctl in ctls)
+            {
+                ctl.MouseMove += OnMouseMove;
+                HookMouseMove(ctl.Controls);
+            }
+        }
+
+        private void OnMouseMove(object sender, MouseEventArgs e)
+        {
+            if (ClientRectangle.Contains(e.Location))
+            {
+                if (settato)
+                {
+                    //double centroX = Screen.PrimaryScreen.Bounds.Width / 2;
+                    //double centroY = Screen.PrimaryScreen.Bounds.Height / 2;
+                    //grade = AngleFrom3PointsInDegrees(Cursor.Position.X, Cursor.Position.Y, centroX, centroY, centroX, centroY);
+                    Point Image = this.PointToClient(JellyMonster.BaseImageLocation);
+                    centroCarroX = Image.X + JellyMonster.GetSize.Width/2;
+                    centroCarroY = Image.Y + JellyMonster.GetSize.Height/ 2;
+                    Point pos = this.PointToClient(Cursor.Position);
+                    grade = AngleFrom3PointsInDegrees(pos.X, pos.Y, centroCarroX, centroCarroY, centroCarroX, centroCarroY);
+                    listBox1.Items.Insert(0, "position MOUSE: X: " + pos.X + "Y: " + pos.Y);
+                    listBox1.Items.Insert(0, "Grade: " + grade.ToString());
+                    /*grade = grade / 10;
+                    if (grade % 10 != 0)
+                        grade -= grade % 10;*/
+                    JellyMonster.ReplaceImage(tank[Convert.ToInt32(grade)]);
+                }
+            }
+        }
+
+
+        private double AngleFrom3PointsInDegrees(double x1, double y1, double x2, double y2, double x3, double y3)
+        {
+            double a = x2 - x1;
+            double b = y2 - y1;
+            double c = x3 - x2;
+            double d = y3 - y2;
+
+            double atanA = Math.Atan2(a, b);
+            double atanB = Math.Atan2(c, d);
+
+            double risultato = (atanA - atanB) * (-180 / Math.PI);
+
+            if (risultato < 0)
+                risultato = 360 + risultato;
+
+            return risultato;
+
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            double xdegree = DegreeToRadian(grade);
+            double cos = -Math.Cos(DegreeToRadian(grade));
+            double sin = Math.Sin(DegreeToRadian(grade));
+            switch (keyData)
+            {
+                case Keys.W:
+                    int xw = Convert.ToInt32(JellyMonster.BaseImageLocation.X + (spostamento * sin));
+                    int yw = Convert.ToInt32(JellyMonster.BaseImageLocation.Y + (spostamento * cos));
+                    listBox1.Items.Insert(0, "position W: X: "+ xw + "Y: " + yw);
+                    JellyMonster.PutBaseImageLocation(new Point(xw, yw));
+                    break;
+                case Keys.S:
+                    int xs = Convert.ToInt32(JellyMonster.BaseImageLocation.X - (spostamento * sin));
+                    int ys = Convert.ToInt32(JellyMonster.BaseImageLocation.Y - (spostamento * cos));
+                    listBox1.Items.Insert(0, "position S: X: " + xs + "Y: " + ys);
+                    JellyMonster.PutBaseImageLocation(new Point(xs, ys));
+                    break;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void mouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                newColpo = true;
+            }
+        }
+
+        private double DegreeToRadian(double angle)
+        {
+            return Math.PI * angle / 180.0;
+        }
+
+        private void tmrSparo_Tick(object sender, EventArgs e)
+        {
+            Graphics formGraphics;
+            SolidBrush myBrush = new SolidBrush(Color.Red);
+            
+            if (newColpo)
+            {
+                puntoInizioSparo = new Point(centroCarroX, centroCarroY);
+                bullets.Add(new Bullet(puntoInizioSparo,0,grade));
+                newColpo = false;
+            }
+            formGraphics = this.CreateGraphics();
+            formGraphics.Clear(Color.Black);
+            Rectangle colpo;
+            foreach(Bullet bullet in bullets)
+            {
+                double xdegree = DegreeToRadian(bullet.grade);
+                double cos = -Math.Cos(DegreeToRadian(bullet.grade));
+                double sin = Math.Sin(DegreeToRadian(bullet.grade));
+
+                colpo = new Rectangle(Convert.ToInt32(bullet.posIniziale.X + (sin * bullet.distanza)), Convert.ToInt32(bullet.posIniziale.Y + (cos * bullet.distanza))
+                    , 5, 5);
+                formGraphics.FillRectangle(myBrush, colpo);
+
+                bullet.distanza += 10;
+            }
+
+        }
+    }
+}
