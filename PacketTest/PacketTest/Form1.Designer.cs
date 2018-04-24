8c7b2fe3fc96c1ee27902a43c78a932115fde56e
PlumberMario <37657115+PlumberMario@users.noreply.github.com>
2018-04-24 09:33:05 +0200

    Add files via upload
    Cartelle con protocollo di rete UDP e progetto per stress test server

---
+++ PacketTest/PacketTest/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace PacketTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(15, 226);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(128, 23);
+            this.btnSend.TabIndex = 0;
+            this.btnSend.Text = "Start sending";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Pacchetti inviati ";
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(103, 6);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(46, 20);
+            this.txtNum.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 41);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(259, 173);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.txtNum);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNum;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ ProtocolloCarroArmato/TankGame/Tank.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankGame
+{
+    public class Tank
+    {
+        private string user;
+        private string color;
+        private string imgPath;
+        private int id;
+        private string address;
+        private Posizione pos;
+
+        public string User
+        {
+            get
+            {
+                return user;
+            }
+
+            set
+            {
+                user = value;
+            }
+        }
+
+        public string Color
+        {
+            get
+            {
+                return color;
+            }
+
+            set
+            {
+                color = value;
+            }
+        }
+
+        public string ImgPath
+        {
+            get
+            {
+                return imgPath;
+            }
+
+            set
+            {
+                imgPath = value;
+            }
+        }
+
+        public Posizione Pos
+        {
+            get
+            {
+                return pos;
+            }
+
+            set
+            {
+                pos = value;
+            }
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Address
+        {
+            get
+            {
+                return address;
+            }
+
+            set
+            {
+                address = value;
+            }
+        }
+
+        public Tank(string user, string color, string imgPath, int iD, Posizione pos)
+        {
+            this.User = user;
+            this.Color = color;
+            this.ImgPath = imgPath;
+            Id = iD;
+            this.Pos = pos;
+        }
+
+        public Tank(string user, string color, string imgPath, int iD, string address, Posizione pos)
+        {
+            this.User = user;
+            this.Color = color;
+            this.ImgPath = imgPath;
+            Id = iD;
+            this.Pos = null;
+            this.Address = address;
+            this.pos = pos;
+        }
+
+        public Tank(string user)
+        {
+            this.User = user;
+        }
+    }
+}
